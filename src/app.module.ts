0ee73939ce5df46c68be1808aad4729128f79c21
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-23 08:49:40 -0300

    implements mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { NcmsModule } from './ncms/ncms.module';
 
 @Module({
-  imports: [NcmsModule],
+  imports: [
+    NcmsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://wendreslucas:NewHorizons7@cluster0.abnnke3.mongodb.net/test',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
