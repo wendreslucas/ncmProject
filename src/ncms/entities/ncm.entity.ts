23f334f1dead53471fb3d0a1523e27f2e6a4f354
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-09-23 08:55:13 -0300

    implements ncm schema

--- src/ncms/entities/ncm.entity.ts
+++ src/ncms/entities/ncm.entity.ts
@@ -1,1 +1,30 @@
-export class Ncm {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type NcmDocument = Ncm & Document;
+
+@Schema()
+export class Ncm {
+  @Prop()
+  codigo: string;
+
+  @Prop()
+  descricao: string;
+
+  @Prop()
+  data_inicio: Date;
+
+  @Prop()
+  data_fim: Date;
+
+  @Prop()
+  tipo_ato: string;
+
+  @Prop()
+  numero_ato: string;
+
+  @Prop()
+  ano_ato: string;
+}
+
+export const NcmSchema = SchemaFactory.createForClass(Ncm);
